8f8f905f0df6908a24ca505c516bd82b01b2c64f
AnisaLateh <anisa.l@ku.th>
2025-09-12 11:51:00 +0700

    delete_exam_result.php

--- delete_exam_result.php
+++ delete_exam_result.php
@@ -3,9 +3,11 @@
 require("connect_db.php");
 if (isset($_GET['student_code'])) {
     $student_code = $_GET['student_code'];
+    $course_code = $_GET['course_code'];
+    
 
-    $stmt = $conn->prepare("DELETE FROM exam_result WHERE student_code = ?");
-    $stmt->bind_param("s", $student_code);
+    $stmt = $conn->prepare("DELETE FROM exam_result WHERE student_code = ? AND course_code= ?");
+    $stmt->bind_param("ss", $student_code,$course_code);
 
     if ($stmt->execute()) {
         header("location: exam_result.php");
